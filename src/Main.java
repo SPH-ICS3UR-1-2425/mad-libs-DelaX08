7aaf12328930b624952963b10a0725e1fe16cb8a
DelaX08 <diana.dela-cruz@stu.ocsb.ca>
2024-09-18 14:38:29 -0400

    Almost done

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,49 @@ public class Main {
 		 */
 		
 		Scanner in = new Scanner(System.in);
-		System.out.println("Give me a country");
+		
+		System.out.println("Give me a name: ");
+		String person1 = in.nextLine(); 
+		System.out.println("Give me another name: ");
+		String person2 = in.nextLine(); 
+		System.out.println("Now, give me an animal: ");
+		String animal = in.nextLine(); 
+		System.out.println("And now, give me an air vehicle: ");
+		String vehicle = in.nextLine(); 
+		System.out.println("This next one is hard.........give me a responsibility you're tired of, like work or school: ");
+		String responsibility = in.nextLine();
+		System.out.println("Give me a day of the week: ");
+		String Dayofweek = in.nextLine(); 
+		System.out.println("Give me a number between 0 and 12.....this number can have decimals: ");
+		double time = in.nextDouble();  
+		System.out.println("You're almost done, but first give me a country: ");
 		String country = in.nextLine(); 
+		in.nextLine();
+		System.out.println("Give me a bizarre job: ");
+		String Weirdjob = in.nextLine(); 
+		System.out.println("Now, give me an international food: ");
+		String food = in.nextLine(); 
+		System.out.println("Last question, give me a name for a pet: ");
+		String petname = in.nextLine(); 
+		
+		System.out.println(); 
+		// On a day morning, person 1 and person 2 were so tired of responsibility that they grabbed their pet animal, pet name and then went to the airport and took the time air vehicle to country. When they arrived, they decided to stay in there forever, working as bizarre job and eating international food.  
+		
+		
+		// person 1 
+		// person 2 
+		// animal 
+		// air vehicle 
+		// responsibility 
+		// day of week 
+		// time --- decimal 
+		// country 
+		// bizarre job 
+		// international food
+		// pet name 
 		
 		
 		
-		System.out.println("");
-		String name = in.nextLine();
-		System.out.println("One day, "+name+" wrote a Mad Lib in class!");
 		
 
 	}
