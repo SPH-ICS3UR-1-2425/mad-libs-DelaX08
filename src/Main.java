4fecb659a21f889dbdfd51a90a8df99eacf37b8b
DelaX08 <diana.dela-cruz@stu.ocsb.ca>
2024-09-19 14:22:49 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -28,19 +28,19 @@ public class Main {
 		String Dayofweek = in.nextLine(); 
 		System.out.println("Give me a number between 0 and 12.....this number can have decimals: ");
 		double time = in.nextDouble();  
-		System.out.println("You're almost done, but first give me a country: ");
-		String country = in.nextLine(); 
 		in.nextLine();
-		System.out.println("Give me a bizarre job: ");
+		System.out.println("You're almost done, but first give me a country: ");
+		String Countryname = in.nextLine(); 
+
+		System.out.println("Give me a bizarre job (Write in in plural) : ");
 		String Weirdjob = in.nextLine(); 
 		System.out.println("Now, give me an international food: ");
 		String food = in.nextLine(); 
 		System.out.println("Last question, give me a name for a pet: ");
 		String petname = in.nextLine(); 
 		
-		System.out.println(); 
-		// On a day morning, person 1 and person 2 were so tired of responsibility that they grabbed their pet animal, pet name and then went to the airport and took the time air vehicle to country. When they arrived, they decided to stay in there forever, working as bizarre job and eating international food.  
-		
+		System.out.println("On a " +Dayofweek+ " morning, " + person1 + " and " + person2 + " were so tired of " + responsibility + " that they took their pet " + animal+ ", " + petname+ ", and then went to the airport, where they took the " + time+ " "+ vehicle + " to " + Countryname + ". When they arrived, they decided to stay in there forever, working as " + Weirdjob+ " and eating " + food+ "."); 
+				
 		
 		// person 1 
 		// person 2 
